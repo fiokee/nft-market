ee58c5a5b7052fe9e9709533b64d96e34b64d1de
fiokee <smleffiong@gmail.com>
2024-09-20 20:16:24 +0100

    commit

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -2,59 +2,63 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTMarketplace is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    
-    struct NFT {
-        uint256 id;
-        address owner;
-        uint256 price;
-        bool forSale;
-    }
-    
-    mapping(uint256 => NFT) public nfts;
+contract NFTMarketplace is ERC721URIStorage, Ownable {
+    uint256 public mintPrice = 0.01 ether;
+    uint256 public totalSupply;
+    mapping(uint256 => uint256) public nftPrices;
+    mapping(uint256 => bool) public listedForSale;
+
+    event NFTMinted(uint256 tokenId, address owner);
+    event NFTListed(uint256 tokenId, uint256 price);
+    event NFTSold(uint256 tokenId, address buyer, uint256 price);
 
     constructor() ERC721("NFTMarketplace", "NFTM") {}
 
-    // Mint a new NFT
-    function mintNFT(string memory tokenURI) public onlyOwner returns (uint256) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        
-        nfts[newItemId] = NFT(newItemId, msg.sender, 0, false);
-        return newItemId;
+    // Minting function
+    function mintNFT(string memory tokenURI) public payable {
+        require(msg.value == mintPrice, "Incorrect minting price");
+
+        totalSupply += 1;
+        uint256 newTokenId = totalSupply;
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        emit NFTMinted(newTokenId, msg.sender);
     }
 
-    // List an NFT for sale
+    // Listing an NFT for sale
     function listNFT(uint256 tokenId, uint256 price) public {
-        require(ownerOf(tokenId) == msg.sender, "Only the owner can list the NFT");
-        nfts[tokenId].price = price;
-        nfts[tokenId].forSale = true;
+        require(ownerOf(tokenId) == msg.sender, "Not the owner");
+        require(price > 0, "Price must be greater than zero");
+
+        nftPrices[tokenId] = price;
+        listedForSale[tokenId] = true;
+
+        emit NFTListed(tokenId, price);
     }
 
     // Buy an NFT
     function buyNFT(uint256 tokenId) public payable {
-        require(nfts[tokenId].forSale, "This NFT is not for sale");
-        require(msg.value == nfts[tokenId].price, "Incorrect price");
+        require(listedForSale[tokenId], "NFT not for sale");
+        uint256 price = nftPrices[tokenId];
+        require(msg.value == price, "Incorrect price");
 
-        address owner = ownerOf(tokenId);
-        payable(owner).transfer(msg.value);
-        _transfer(owner, msg.sender, tokenId);
+        address seller = ownerOf(tokenId);
 
-        nfts[tokenId].owner = msg.sender;
-        nfts[tokenId].forSale = false;
+        _transfer(seller, msg.sender, tokenId);
+        payable(seller).transfer(msg.value);
+
+        listedForSale[tokenId] = false;
+
+        emit NFTSold(tokenId, msg.sender, price);
     }
 
-    // Transfer ownership
-    function transferNFT(uint256 tokenId, address newOwner) public {
-        require(ownerOf(tokenId) == msg.sender, "Only the owner can transfer");
-        _transfer(msg.sender, newOwner, tokenId);
-        nfts[tokenId].owner = newOwner;
+    // Only owner can update mint price
+    function setMintPrice(uint256 newPrice) public onlyOwner {
+        mintPrice = newPrice;
     }
 }
